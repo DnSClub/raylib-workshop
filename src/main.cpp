5f7c4b9f8d1c4806b80232323750901a2111cc0c
Saarang <saaranganand2013@gmail.com>
2024-09-24 22:09:15 -0700

    Workshop Content

--- src/main.cpp
+++ src/main.cpp
@@ -1,67 +1,221 @@
-/*******************************************************************************************
-*
-*   raylib [core] example - Basic window
-*
-*   Welcome to raylib!
-*
-*   To test examples, just press F6 and execute raylib_compile_execute script
-*   Note that compiled executable is placed in the same folder as .c file
-*
-*   You can find all basic examples on C:\raylib\raylib\examples folder or
-*   raylib official webpage: www.raylib.com
-*
-*   Enjoy using raylib. :)
-*
-*   Example originally created with raylib 1.0, last time updated with raylib 1.0
-*
-*   Example licensed under an unmodified zlib/libpng license, which is an OSI-certified,
-*   BSD-like license that allows static linking with closed source software
-*
-*   Copyright (c) 2013-2024 Ramon Santamaria (@raysan5)
-*
-********************************************************************************************/
-
+#include <iostream>
 #include "raylib.h"
+#include <deque>
+#include "raymath.h"
 
-//------------------------------------------------------------------------------------
-// Program main entry point
-//------------------------------------------------------------------------------------
-int main(void)
-{
-    // Initialization
-    //--------------------------------------------------------------------------------------
-    const int screenWidth = 800;
-    const int screenHeight = 450;
+int cellSize = 30;
+int cellCount = 25;
+int offset = 75;
 
-    InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window");
+double lastUpdateTime = 0;
 
-    SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
-    //--------------------------------------------------------------------------------------
-
-    // Main game loop
-    while (!WindowShouldClose())    // Detect window close button or ESC key
+bool TimePassed(double interval) {
+    double currentTime = GetTime();
+    if(currentTime - lastUpdateTime >= interval)
     {
-        // Update
-        //----------------------------------------------------------------------------------
-        // TODO: Update your variables here
-        //----------------------------------------------------------------------------------
+        lastUpdateTime = currentTime;
+        return true;
+    }
+    return false;
+}
 
-        // Draw
-        //----------------------------------------------------------------------------------
+bool ElementInDeque(Vector2 element, std::deque<Vector2> deque)
+{
+    for(int i = 0; i < deque.size(); i++)
+    {
+        if(Vector2Equals(element, deque[i]))
+        {
+            return true;
+        }
+    }
+    return false;
+}
+
+class Food {
+    public:
+        Vector2 position;
+        Texture2D texture;
+
+        Food(std::deque<Vector2> snakeBody) {
+            Image img = LoadImage("graphics/apple.png");
+            texture = LoadTextureFromImage(img);
+            UnloadImage(img);
+            position = GenerateRandomPos(snakeBody);
+        }
+
+        ~Food() {
+            UnloadTexture(texture);
+        }
+
+        void Draw() {
+            // DrawRectangle(position.x * cellSize, position.y * cellSize, cellSize, cellSize, DARKGREEN);
+            DrawTexture(texture, offset + position.x * cellSize, offset + position.y * cellSize, WHITE);
+        }
+
+        Vector2 GenerateRandomCell() {
+            float x = GetRandomValue(0, cellCount - 1);
+            float y = GetRandomValue(0, cellCount - 1);
+            return Vector2{x,y};
+        }
+
+        Vector2 GenerateRandomPos(std::deque<Vector2> snakeBody) {
+            Vector2 position = GenerateRandomCell();
+            while(ElementInDeque(position, snakeBody))
+            {
+                position = GenerateRandomCell();
+            }
+            return position;
+        }
+};
+
+class Snake {
+    public:
+        std::deque<Vector2> body = {Vector2{6,9}, Vector2{5,9}, Vector2{4,9}};
+        Vector2 direction = {1,0};
+        bool addSegment = false;
+
+        void Draw() {
+            for(int i = 0; i < body.size(); i++)
+            {
+                float x = body[i].x;
+                float y = body[i].y;
+                Rectangle rec = Rectangle{offset + x * cellSize, offset + y * cellSize, (float)cellSize, (float)cellSize};
+                DrawRectangleRounded(rec, 0.5, 6, DARKGREEN);
+            }
+        }
+
+        void Update() {
+            body.push_front(Vector2Add(body[0], direction));
+            if(addSegment)
+            {
+                addSegment = false;
+            }
+            else {
+                body.pop_back();
+            }
+        }
+
+        void Reset() {
+            body = {Vector2{6,9}, Vector2{5,9}, Vector2{4,9}};
+            direction = {1,0};
+        }
+};
+
+class Game {
+    public:
+        Snake snake = Snake();
+        Food food = Food(snake.body);
+        bool isRunning = true;
+        int score = 0;
+        Sound eat;
+        Sound wall;
+
+        Game() {
+            InitAudioDevice();
+            eat = LoadSound("sounds/eat.mp3");
+            wall = LoadSound("sounds/wall.mp3");
+        }
+
+        ~Game() {
+            UnloadSound(eat);
+            UnloadSound(wall);
+            CloseAudioDevice();
+        }
+
+        void Draw() {
+            food.Draw();
+            snake.Draw();
+        }
+
+        void Update() {
+            if(isRunning) {
+                snake.Update();
+                checkCollisionWithFood();
+                checkCollisionWithEdge();
+                checkCollisionWithItself();
+            }
+        }
+
+        void checkCollisionWithFood() {
+            if(Vector2Equals(snake.body[0], food.position))
+            {
+                food.position = food.GenerateRandomPos(snake.body);
+                snake.addSegment = true;
+                score++;
+                PlaySound(eat);
+            }
+        }
+
+        void checkCollisionWithEdge() {
+            if(snake.body[0].x == cellCount || snake.body[0].x == -1
+            || snake.body[0].y == cellCount || snake.body[0].y == -1)
+            {
+                GameOver();
+            }
+        }
+
+        void checkCollisionWithItself() {
+            std::deque<Vector2> headlessSnake = snake.body;
+            headlessSnake.pop_front();
+            if(ElementInDeque(snake.body[0], headlessSnake))
+            {
+                GameOver();
+            }
+        }
+
+        void GameOver() {
+            snake.Reset();
+            food.position = food.GenerateRandomPos(snake.body);
+            isRunning = false;
+            score = 0;
+            PlaySound(wall);
+        }
+};
+
+int main() {
+    InitWindow(2*offset + cellSize * cellCount, 2*offset + cellSize * cellCount, "Snake");
+    SetTargetFPS(60);
+
+    Game game = Game();
+    
+    while (WindowShouldClose() == false)
+    {
         BeginDrawing();
 
-            ClearBackground(RAYWHITE);
+        if(TimePassed(0.2))
+            game.Update();
 
-            DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
+        if(IsKeyPressed(KEY_W) && game.snake.direction.y != 1)
+        {
+            game.snake.direction = {0,-1};
+            game.isRunning = true;
+        }
+        if(IsKeyPressed(KEY_A) && game.snake.direction.x != 1)
+        {
+            game.snake.direction = {-1,0};
+            game.isRunning = true;
+        }
+        if(IsKeyPressed(KEY_S) && game.snake.direction.y != -1)
+        {
+            game.snake.direction = {0,1};
+            game.isRunning = true;
+        }
+        if(IsKeyPressed(KEY_D) && game.snake.direction.x != -1)
+        {
+            game.snake.direction = {1,0};
+            game.isRunning = true;
+        }
+
+        ClearBackground(GREEN);
+        Rectangle rec = Rectangle{(float)offset-5, (float)offset-5, (float)cellSize*cellCount+10, (float)cellSize*cellCount+10};
+        DrawRectangleLinesEx(rec, 5, DARKGREEN);
+        DrawText("DNS Raylib Workshop Snake", offset-5, 20, 40, DARKGREEN);
+        DrawText(TextFormat("%d", game.score), offset+cellSize*cellCount-15, 20, 40, DARKGREEN);
+        game.Draw();
 
         EndDrawing();
-        //----------------------------------------------------------------------------------
     }
-
-    // De-Initialization
-    //--------------------------------------------------------------------------------------
-    CloseWindow();        // Close window and OpenGL context
-    //--------------------------------------------------------------------------------------
-
+    
+    CloseWindow();
     return 0;
 }
